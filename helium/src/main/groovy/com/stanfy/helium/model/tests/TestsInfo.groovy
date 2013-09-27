bfed24c0fef23dcc43cade07becd563bf2296cf1
Roman Mazur <mazur.roman@gmail.com>
2013-09-27 16:36:24 +0300

    http headers in tests info

--- helium/src/main/groovy/com/stanfy/helium/model/tests/TestsInfo.groovy
+++ helium/src/main/groovy/com/stanfy/helium/model/tests/TestsInfo.groovy
@@ -1,11 +1,17 @@
 package com.stanfy.helium.model.tests
 
+import groovy.transform.CompileStatic
+
 /**
  * Tests information.
  */
+@CompileStatic
 class TestsInfo {
 
   /** Whether to use examples. */
   Boolean useExamples
+
+  /** Set of predefined HTTP headers. */
+  Map<String, Object> httpHeaders = new LinkedHashMap<>()
 
 }
