d77f0eb49932e2cead31af9ec4376864a18347f9
Roman Mazur <mazur.roman@gmail.com>
2013-09-27 19:54:25 +0300

    Fail on 405

--- helium/src/main/groovy/com/stanfy/helium/handler/codegen/tests/RestApiMethods.java
+++ helium/src/main/groovy/com/stanfy/helium/handler/codegen/tests/RestApiMethods.java
@@ -74,8 +74,12 @@ public class RestApiMethods {
     String reason = status != null && status.length() > 0 ? " Got '" + status + "'" : "";
 
     assertThat(response.getStatusLine().getStatusCode())
-        .describedAs("Method not found... Maybe not implemented yet?")
+        .describedAs("Method not found... Maybe not implemented yet?" + reason)
         .isNotEqualTo(HttpStatus.SC_NOT_FOUND);
+
+    assertThat(response.getStatusLine().getStatusCode())
+        .describedAs("Incorrect HTTP method" + reason)
+        .isNotEqualTo(HttpStatus.SC_METHOD_NOT_ALLOWED);
 
     if (success) {
       log("Validating successful status...");
