18893699f2ef22079e6eddabc56f06d2cbf0a0ba
Roman Mazur <rmazur@stanfy.com.ua>
2013-10-22 16:12:48 +0300

    set version constant

--- gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
+++ gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory
  */
 class HeliumExtension {
 
-  public static final String VERSION = "0.1-SNAPSHOT"
+  public static final String VERSION = "0.2-SNAPSHOT"
 
   /** Tasks group. */
   public static final String GROUP = "Helium"
