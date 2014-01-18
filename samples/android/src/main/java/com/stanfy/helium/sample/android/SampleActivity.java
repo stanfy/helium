618dab4d129f7346cce5d7f458bbcd528ef209bb
Roman Mazur <rmazur@stanfy.com.ua>
2014-01-18 18:48:40 +0200

    Android sample

---
+++ samples/android/src/main/java/com/stanfy/helium/sample/android/SampleActivity.java
@@ -0,0 +1,16 @@
+package com.stanfy.helium.sample.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Sample activity.
+ */
+public class SampleActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+  }
+}
