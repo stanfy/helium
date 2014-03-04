2a022d512ec275d792285e60ca9b69347baa739a
Alexandr Tereshchuk <atereshchuk@stanfy.com.ua>
2014-03-04 16:32:46 +0200

    generate safe field names for reserved java keywods

--- helium/src/main/groovy/com/stanfy/helium/handler/codegen/java/entity/PojoWriter.java
+++ helium/src/main/groovy/com/stanfy/helium/handler/codegen/java/entity/PojoWriter.java
@@ -8,7 +8,9 @@ import com.stanfy.helium.model.Message;
 import javax.lang.model.element.Modifier;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -38,20 +40,20 @@ public class PojoWriter implements JavaClassWriter {
 
   @Override
   public void writeField(final Field field, final String fieldTypeName, final String fieldName, final Set<Modifier> modifiers) throws IOException {
-    output.emitField(fieldTypeName, fieldName, modifiers);
+    output.emitField(fieldTypeName, safeFieldName(fieldName), modifiers);
   }
 
   @Override
   public void writeSetterMethod(final Field field, final String fieldTypeName, final String accessorName, final String fieldName) throws IOException {
     output.beginMethod("void", accessorName, Collections.singleton(Modifier.PUBLIC), fieldTypeName, "value");
-    output.emitStatement("this.%s = value", fieldName);
+    output.emitStatement("this.%s = value", safeFieldName(fieldName));
     output.endMethod();
   }
 
   @Override
   public void writeGetterMethod(final Field field, final String fieldTypeName, final String accessorName, final String fieldName) throws IOException {
     output.beginMethod(fieldTypeName, accessorName, Collections.singleton(Modifier.PUBLIC));
-    output.emitStatement("return this.%s", fieldName);
+    output.emitStatement("return this.%s", safeFieldName(fieldName));
     output.endMethod();
   }
 
@@ -69,5 +71,26 @@ public class PojoWriter implements JavaClassWriter {
   public void writeConstructors(final Message message) {
     // nothing
   }
+
+  private static String safeFieldName(final String fieldName) {
+    if (JAVA_KEYWORDS.contains(fieldName)) {
+      return fieldName.concat("Field");
+    }
+    return fieldName;
+  }
+
+  /** Reserved java keywords. */
+  private static final Set<String> JAVA_KEYWORDS = new HashSet<String>(Arrays.asList(
+    "abstract",	"continue",	"for", "new",	"switch",
+    "assert",  "default",  "goto",  "package",  "synchronized",
+    "boolean",  "do",  "if",  "private",  "this",
+    "break",  "double",  "implements",  "protected",  "throw",
+    "byte",  "else",  "import",  "public",  "throws",
+    "case",  "enum",  "instanceof",  "return",  "transient",
+    "catch",  "extends",  "int",  "short",  "try",
+    "char",  "final",  "interface",  "static",  "void",
+    "class",  "finally",  "long",  "strictfp",  "volatile",
+    "const",  "float",  "native",  "super",  "while"
+  ));
 
 }
