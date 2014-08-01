f67ea716efc1e200c5c7bf5a5e9018734fcccd26
Roman Mazur <rmazur@stanfy.com.ua>
2014-08-01 20:32:17 +0300

    next dev version

--- gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
+++ gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
@@ -13,7 +13,7 @@ import static com.stanfy.helium.gradle.UserConfig.specName
  */
 class HeliumExtension {
 
-  public static final String VERSION = "0.5.3"
+  public static final String VERSION = "0.5.4-SNAPSHOT"
 
   /** Tasks group. */
   public static final String GROUP = "Helium"
