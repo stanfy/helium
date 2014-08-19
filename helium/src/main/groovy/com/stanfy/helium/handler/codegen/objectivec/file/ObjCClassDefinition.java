b75ed8d6c72ce722b262c078051d454953035b87
Paul Taykalo <tt.kilew@gmail.com>
2014-08-20 00:07:35 +0300

    Properties adde dot the class Definition

--- helium/src/main/groovy/com/stanfy/helium/handler/codegen/objectivec/file/ObjCClassDefinition.java
+++ helium/src/main/groovy/com/stanfy/helium/handler/codegen/objectivec/file/ObjCClassDefinition.java
@@ -1,5 +1,8 @@
 package com.stanfy.helium.handler.codegen.objectivec.file;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ptaykalo on 8/17/14.
  * Holds information about class Definition for spcific obejctive class with specific ClassName
@@ -9,6 +12,7 @@ public class ObjCClassDefinition implements ObjCSourcePart {
 Class Name
  */
   private String className;
+  private ArrayList<ObjCPropertyDefinition>propertyDefinitions = new ArrayList<ObjCPropertyDefinition>();
 
   public ObjCClassDefinition(final String className) {
     this.className = className;
@@ -26,5 +30,16 @@ Class Name
             "@interface " + className + " : NSObject",
             "@end"
         );
+  }
+
+  /*
+  Adds spcecified property definition to this class
+   */
+  public void addPropertyDefinition(ObjCPropertyDefinition property) {
+    propertyDefinitions.add(property);
+  }
+
+  public List<ObjCPropertyDefinition> getPropertyDefinitions() {
+    return propertyDefinitions;
   }
 }
