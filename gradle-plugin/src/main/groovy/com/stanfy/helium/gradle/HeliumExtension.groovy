d139c3c0ee9ec2571326b3ae6b83d48fff8004d5
Roman Mazur <rmazur@stanfy.com.ua>
2015-03-02 11:51:18 +0200

    Set version 0.5.5

--- gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
+++ gradle-plugin/src/main/groovy/com/stanfy/helium/gradle/HeliumExtension.groovy
@@ -13,7 +13,7 @@ import static com.stanfy.helium.gradle.UserConfig.specName
  */
 class HeliumExtension {
 
-  public static final String VERSION = "0.5.5-SNAPSHOT"
+  public static final String VERSION = "0.5.5"
 
   /** Tasks group. */
   public static final String GROUP = "Helium"
